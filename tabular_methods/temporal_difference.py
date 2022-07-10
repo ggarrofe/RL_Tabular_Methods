c75dd993016af24be1c87f3a94d628828123795a
Guillem Garrofe <guillemgarrofemontoliu@gmail.com>
2022-07-10 13:58:53 +0100

    td updated

--- tabular_methods/temporal_difference.py
+++ tabular_methods/temporal_difference.py
@@ -1,10 +1,11 @@
 import numpy as np
+from tqdm.notebook import tqdm
 
 # This class define the Temporal-Difference agent
 
-class TD_agent(object):
+class Qlearning_agent(object):
 
-    def solve(self, env):
+    def solve(self, env, total_episodes=1000, alpha=0.9):
         """
         Solve a given Maze environment using Temporal Difference learning
         input: env {Maze object} -- Maze to solve
@@ -13,10 +14,6 @@ class TD_agent(object):
           - values {list of np.array} -- List of successive value functions for each episode 
           - total_rewards {list of float} -- Corresponding list of successive total non-discounted sum of reward for each episode 
         """
-
-        # Initialisation (can be edited)
-        TOTAL_EPISODES = 1000
-        alpha = 0.9
         E = 0.3
         Q = np.random.rand(env.get_state_size(), env.get_action_size())
         
@@ -33,9 +30,7 @@ class TD_agent(object):
         ####
         n_episodes = 0
         
-        while n_episodes < TOTAL_EPISODES:
-            print("Solving episode %d" % (n_episodes), end='\r')
-            
+        for n_episodes in tqdm(range(total_episodes), unit="episode", leave=False):
             total_reward = 0.0
             
             t, state, reward, done = env.reset()
@@ -67,6 +62,64 @@ class TD_agent(object):
             total_rewards.append(total_reward)
             n_episodes += 1
             
-        print()
+        
+        return policy, values, total_rewards
+
+class SARSA_agent(object):
+
+    def solve(self, env, total_episodes=1000, alpha=0.9):
+        """
+        Solve a given Maze environment using Temporal Difference learning
+        input: env {Maze object} -- Maze to solve
+        output: 
+          - policy {np.array} -- Optimal policy found to solve the given Maze environment 
+          - values {list of np.array} -- List of successive value functions for each episode 
+          - total_rewards {list of float} -- Corresponding list of successive total non-discounted sum of reward for each episode 
+        """
+        E = 0.1
+        Q = np.random.rand(env.get_state_size(), env.get_action_size())
+        
+        policy = np.random.rand(env.get_state_size(), env.get_action_size())
+        policy /= np.sum(policy, axis=1)[:,None] 
+        policy = np.ones((env.get_state_size(), env.get_action_size()))*0.25
+        
+        values = []
+        total_rewards = []
+        
+        ####
+        # On-policy TD control: SARSA
+        #
+        ####
+        for n_episodes in tqdm(range(total_episodes), unit="episode", leave=False):
+            
+            total_reward = 0.0
+            
+            t, state, reward, done = env.reset()
+            action = np.random.choice(list(range(4)), p=policy[state])
+
+            if type(alpha) == str and alpha == 'Episode Inverse Decay':
+                alpha_t = 1/(n_episodes+1)
+            
+            while not done:
+                t, next_state, reward, done = env.step(action)
+                total_reward += reward
+
+                next_action = np.random.choice(list(range(4)), p=policy[next_state])
+                Q[state, action] = Q[state, action] + alpha_t * (reward + env.get_gamma()*Q[next_state, next_action] - Q[state, action])
+                
+                # Update e-greedy policy
+                a_opt = np.argmax(Q[state,:])
+                for a in range(env.get_action_size()):
+                    if a == a_opt:
+                        policy[state][a] = 1 - E + E/env.get_action_size()
+                    else:
+                        policy[state][a] = E/env.get_action_size()
+
+                state, action = next_state, next_action
+            
+            values.append(np.sum(policy*Q, axis=1))
+            total_rewards.append(total_reward)
+            n_episodes += 1
+            
         
         return policy, values, total_rewards
--- utils/utils.py
+++ utils/utils.py
@@ -1,18 +1,19 @@
 import pandas as pd
 
-def append_results(total_rewards, iteration, df=None, epsilon=None, alpha=None, mse=None):
+def append_results(total_rewards, iteration=None, df=None, epsilon=None, alpha=None, mse=None, classifier=None):
     
     results = {
         "Total rewards": total_rewards, 
-        "Episodes": list(range(1, len(total_rewards)+1)), 
-        "Iteration": [iteration]*len(total_rewards),
+        "Episodes": list(range(1, len(total_rewards)+1))
     }
 
+    if iteration is not None: results["Iteration"] = [iteration]*len(total_rewards)
     if epsilon is not None: results["Epsilon"] = [epsilon]*len(total_rewards)
     if alpha is not None: results["Alpha"] = [alpha]*len(total_rewards)
     if epsilon is not None: results["epsilon"] = [epsilon]*len(total_rewards)
     if mse is not None: results["MSE"] = mse
-    
+    if classifier is not None: results["Classifier"] = [classifier]*len(total_rewards)
+
     try:
         df = df.append(pd.DataFrame(results), ignore_index=True)
     except (NameError, AttributeError) as e:
